51fa4dc7fdeba1e8660092634033ba05fe2799df
michaelstsnow <michael.st.snow@gmail.com>
2018-11-26 19:01:46 +0000

    Done.

---
+++ lib/author.rb
@@ -0,0 +1,30 @@
+class Author
+  attr_accessor(:name)
+  @@posts=[]
+
+  def initialize(name)
+    @name=name
+    @posts=[]
+  end
+
+  def posts
+    @posts
+  end
+
+  def add_post(post)
+    post.author=self
+    @posts << post
+    @@posts << post
+  end
+
+  def add_post_by_title (title)
+    post = Post.new(title)
+    post.author = self
+    @posts << post
+    @@posts << post
+  end
+
+  def self.post_count
+    @@posts.length
+  end
+end
